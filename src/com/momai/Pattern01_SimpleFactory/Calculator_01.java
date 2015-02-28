7f0218217e6bcfbca00cfd3af65201ed3842332f
lian819 <lianxinzhong@126.com>
2015-02-28 21:15:58 +0800

    SimpleFactory
    简单工厂模式

---
+++ src/com/momai/Pattern01_SimpleFactory/Calculator_01.java
@@ -0,0 +1,19 @@
+package com.momai.Pattern01_SimpleFactory;
+
+/**
+ * @author 	Administrator
+ * @date	2015年2月27日 下午5:11:43
+ * @desc	没有使用面向对象的简易计算器
+ */
+public class Calculator_01 {
+	
+	public static void main(String[] args) {
+		try {
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			
+			System.out.println("您的输入有错!");
+		}
+	}
+}
